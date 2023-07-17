bb78eb219cc9ff292682bbbaa30bd8cc473ce16d
Weslley <106591789+Weslleyso@users.noreply.github.com>
2023-07-17 11:08:55 -0300

    Update Word.py

--- AnalisadorLexico/Word.py
+++ AnalisadorLexico/Word.py
@@ -5,20 +5,31 @@ TAG = 1
 def create_token(lexeme, tag):
     return (lexeme, tag)
 
-# Tokens
-AND = create_token("&&", 256)
-OR = create_token("||", 257)
-EQ = create_token("==", 258)
-NE = create_token("!=", 259)
-LE = create_token("<=", 260)
-GE = create_token(">=", 261)
-MINUS = create_token("minus", 262)
-TRUE = create_token("True", 263)
-FALSE = create_token("False", 264)
-TEMP = create_token("t", 265)
+# Dicionário de tags e lexemas
+tags = {
+    'AND': create_token("&&", 256),
+    'BASIC': create_token("BASIC", 257),
+    'BREAK': create_token("BREAK", 258),
+    'DO': create_token("DO", 259),
+    'ELSE': create_token("ELSE", 260),
+    'EQ': create_token("EQ", 261),
+    'FALSE': create_token("FALSE", 262),
+    'GE': create_token("GE", 263),
+    'ID': create_token("ID", 264),
+    'IF': create_token("IF", 265),
+    'INDEX': create_token("INDEX", 266),
+    'LE': create_token("LE", 267),
+    'MINUS': create_token("MINUS", 268),
+    'NE': create_token("NE", 269),
+    'NUM': create_token("NUM", 270),
+    'OR': create_token("OR", 271),
+    'REAL': create_token("REAL", 272),
+    'TEMP': create_token("TEMP", 273),
+    'TRUE': create_token("TRUE", 274),
+    'WHILE': create_token("WHILE", 275)
+}
 
-# Exemplo de uso.
-#Lembrar de tirar essa parte na hora de implementar!
-my_token = AND
+# Exemplo de uso
+my_token = tags['REAL']
 print(my_token[LEXEME])  # Saída: "&&"
 print(my_token[TAG])  # Saída: 256
