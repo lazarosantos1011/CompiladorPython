3f80c609fffb1f376656f190aed4107cc528804d
Weslley <106591789+Weslleyso@users.noreply.github.com>
2023-07-17 11:25:25 -0300

    Update Lexer.py

--- AnalisadorLexico/Lexer.py
+++ AnalisadorLexico/Lexer.py
@@ -35,3 +35,77 @@ def readch(c):
         return False
     peek = ''
     return True
+#FALTA TERMINAR
+def scan():
+    while True:
+        if peek == '\n':
+            line += 1
+        else:
+            break
+
+    if peek == '&':
+        if readch('&'):
+            return Word.and_token
+        else:
+            return Token('&')
+    elif peek == '|':
+        if readch('|'):
+            return Word.or_token
+        else:
+            return Token('|')
+    elif peek == '=':
+        if readch('='):
+            return Word.eq_token
+        else:
+            return Token('=')
+    elif peek == '!':
+        if readch('='):
+            return Word.ne_token
+        else:
+            return Token('!')
+    elif peek == '<':
+        if readch('='):
+            return Word.le_token
+        else:
+            return Token('<')
+    elif peek == '>':
+        if readch('='):
+            return Word.ge_token
+        else:
+            return Token('>')
+
+    if peek.isdigit():
+        v = 0
+        while peek.isdigit():
+            v = 10 * v + int(peek)
+            readch()
+
+        if peek != '.':
+            return Num(v)
+        
+        x = float(v)
+        d = 10.0
+
+        while True:
+            readch()
+            if not peek.isdigit():
+                break
+            x = x + int(peek) / d
+            d = d * 10.0
+        
+        return Real(x)
+
+    if peek.isalpha():
+        b = ''
+        while peek.isalnum():
+            b += peek
+            readch()
+
+        s = b
+        w = Word.get(s)
+        if w is not None:
+            return w
+        else:
+            w = Word(s, Tag.ID)
+            return w
+
