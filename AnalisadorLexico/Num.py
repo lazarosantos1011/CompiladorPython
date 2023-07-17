6c4601cc1271efc18057b84ee2255823edc0b38f
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-07-17 10:37:12 -0300

    Update Num.py

--- AnalisadorLexico/Num.py
+++ AnalisadorLexico/Num.py
@@ -3,12 +3,17 @@
 NUM = 'NUM'
 
 def create_num_token(value):
-    return (NUM, value)
+    if not isinstance(value, int):
+        raise ValueError("Num tokens must have integer values")
+    return { 'tag': NUM, 'value': value }
 
 def num_to_string(token):
-    return str(token[1])
+    return str(token['value'])
 
-# Usage example
-token = create_num_token(42)  # Create a token with tag 'NUM' and value 42
-token_string = num_to_string(token)  # Convert the token to a string
-print(token_string)  # Output: '42'
+# Exemplo
+
+token1 = create_num_token(int(input("Digite um inteiro: ")))  # Cria token com a tag "NUM" e valor inteiro 42
+token_string1 = num_to_string(token1)  # Converte o token para string
+print(token_string1)  # Saída: '42'
+
+token2 = create_num_token(int(input("Digite um inteiro: ")))  # Gera um ValueError pois 3.14 não é um inteiro
