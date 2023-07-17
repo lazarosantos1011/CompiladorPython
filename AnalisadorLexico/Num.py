a2b675c29daf4f9944b454a403effde7de0d60f7
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2023-07-17 10:11:22 -0300

    Add files via upload

--- AnalisadorLexico/Num.py
+++ AnalisadorLexico/Num.py
@@ -1,10 +1,14 @@
-#num.py
-from Token import Token
+# Arquivo Num.py
 
-class Num(Token):
-    def __init__(self, v):
-        super().__init__(tag.NUM)
-        self.value = v
+NUM = 'NUM'
 
-    def __str__(self):
-        return str(self.value)
+def create_num_token(value):
+    return (NUM, value)
+
+def num_to_string(token):
+    return str(token[1])
+
+# Usage example
+token = create_num_token(42)  # Create a token with tag 'NUM' and value 42
+token_string = num_to_string(token)  # Convert the token to a string
+print(token_string)  # Output: '42'
